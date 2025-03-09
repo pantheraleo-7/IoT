64fd11f1db7a4787e621983cf5a597ef01b6b1ba
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2025-03-09 20:39:50 +0530

    add p4

---
+++ practical-4.py
@@ -0,0 +1,23 @@
+from gpiozero import LED
+# pip install python-telegram-bot
+from telegram.ext import Application, CommandHandler
+
+
+TOKEN = ""
+led = LED(13)
+
+
+async def turn_on(update, context):
+    led.on()
+    await update.message.reply_text("LED is now ON 💡")
+
+
+async def turn_off(update, context):
+    led.off()
+    await update.message.reply_text("LED is now OFF 🌑")
+
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("on", turn_on))
+app.add_handler(CommandHandler("off", turn_off))
+app.run_polling()
