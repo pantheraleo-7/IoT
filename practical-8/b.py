7db07f8c5517223f00c43403270b9085402684af
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2025-03-09 19:38:50 +0530

    add p5, p10; change p6, p9; refactor

--- practical-8/b.py
+++ practical-8/b.py
@@ -2,22 +2,24 @@ import time
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 
 
 picam = Picamera2()
+encoder = H264Encoder(bitrate=5_000_000)
+output = FfmpegOutput("video.mp4")
 
 vconfig = picam.create_video_configuration()
 picam.configure(vconfig)
-encoder = H264Encoder(bitrate=5_000_000)
-print("Smile, you're on camera.")
-picam.start_preview()
-picam.start_recording(encoder, "vid.h264")
+print("Smile, you're on camera")
+picam.start_preview(True)
+picam.start_recording(encoder, output)
 time.sleep(10)
 picam.stop_preview()
 picam.stop_recording()
-print('Video captured.')
+print("Video captured")
 
 # using convenience method
-print("Smile, you're on camera.")
-picam.start_and_record_video("vid.mp4", duration=10, show_preview=True)
-print('Video captured.')
+# print("Smile, you're on camera")
+# picam.start_and_record_video("video.mp4", duration=10, show_preview=True)
+# print("Video captured")
