1bcda8ffa0814b214703cd19749fdff970a3041f
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2025-03-10 00:38:08 +0530

    add p7

---
+++ practical-7.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from gpiozero import LED
+
+
+app = FastAPI()
+led = LED(13)
+
+
+@app.post("/led/toggle")
+def toggle_led(on: bool):
+    if on:
+        led.on()
+    else:
+        led.off()
+
+    return {"status": on}
