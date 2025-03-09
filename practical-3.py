7937e1a9dae81e818885a7194dba05253184733a
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2025-03-09 19:38:50 +0530

    complete

--- practical-3.py
+++ practical-3.py
@@ -3,20 +3,21 @@ from collections import deque
 import board
 import numpy as np
 import matplotlib.pyplot as plt
+from adafruit_ads1x15 import ads1x15
 from adafruit_ads1x15.ads1115 import ADS1115
-from adafruit_ads1x15.ads1x15 import Mode
 from adafruit_ads1x15.analog_in import AnalogIn
 from matplotlib.animation import FuncAnimation
 
 
 DURATION = 10
-SAMPLE_RATE = 16 # powers of 2
+SAMPLE_RATE = 16 # 8, 16, 32, 64, 128, 250, 475, 860
 GAIN = 4 # 2/3, 1, 2, 4, 8, 16
-CHANNELS = (0, None) # 0, 1, 2, 3
+CHANNELS = (0, None) # Single-ended read
+#CHANNELS = (0, 1) # Differential read between channels 0 and 1
 
 i2c = board.I2C()
-ads = ADS1115(i2c, GAIN, SAMPLE_RATE, Mode.CONTINUOUS)
-chan = AnalogIn(ads, *CHANNELS)
+ads = ADS1115(i2c, GAIN, SAMPLE_RATE, ads1x15.Mode.CONTINUOUS)
+chan = AnalogIn(ads, *CHANNELS) # ADS1115 has 4 input channels: 0, 1, 2, 3
 
 num_samples = SAMPLE_RATE*DURATION
 x = np.linspace(0, DURATION, num_samples)
@@ -29,14 +30,16 @@ ax.set_ylim(-7, 7)
 ax.set_xlabel("Time [s]")
 ax.set_ylabel("Voltage [V]")
 ax.set_title("Real-Time Oscilloscope Simulation")
-line, = ax.plot(x, y, marker='o', markevery=[-1])
+line, = ax.plot(x, y, marker="o", markevery=[-1])
+
 
 def update(_frame):
     y.popleft()
     y.append(chan.voltage)
-
     line.set_ydata(y)
+
     return line,
+
 
 anim = FuncAnimation(fig, update, frames=num_samples, interval=1000/SAMPLE_RATE, blit=True)
 plt.show()
